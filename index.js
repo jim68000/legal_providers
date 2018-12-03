80dae5ac832160e63c65b32df10456d29cda5e36
jimsmith <jim.smith@gmail.com>
2018-12-03 12:31:17 +0000

    add route for reading files

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const parse = require('csv-parse/lib/sync')
 const fs = require('fs')
+const _ = require('lodash')
 
 const legal_providers = parse(fs.readFileSync('./legal-providers.csv', 'UTF-8'), {columns: true})
-// ids start at 1, so we insert an empty element to align ids and index
-legal_providers.unshift(null)
 
+const express = require('express')
+const app = express()
+
+app.get('/providers', (req, res) => {
+  if (_.isEmpty(req.query)) {
+    res.json(legal_providers)
+  } else {
+    const sought_keys = Object.keys(req.query)
+    filtered = legal_providers
+    sought_keys.forEach((key) => {
+      filtered = filtered.filter((l) => l[key] === req.query[key])
+    })
+    if (filtered.length === 0) {
+      res.status(404).send('No matching providers')
+    }
+    res.json(filtered)
+  }
+})
+
+app.get('/provider/:id?', (req, res) => {
+  // deal with off by one diff between file and array representation
+  const idx = parseInt(req.params.id, 10) - 1
+  res.json(legal_providers[idx])
+})
+
+app.listen(3000, () => console.log('app started at 3000'))
